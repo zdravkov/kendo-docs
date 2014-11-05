5627d5dba69b663f44e48f5e866126ce6bcb408f
Atanas Korchev <akorchev@gmail.com>
2014-11-05 17:51:24 +0200

    Allow html fenced code blocks to be editable in the Dojo.

--- _assets/javascripts/app.js
+++ _assets/javascripts/app.js
@@ -99,20 +99,82 @@ $(function(){
     });
 });
 
+var dojoApi = (function($) {
+
+    var dojoApi = {
+        configuration: {
+            url: "http://dojo.telerik.com/",
+            currentKendoVersion: kendo.version
+        },
+        post: function (snippet) {
+            if (!snippet.match(/<html>/i)) {
+                snippet = dojoApi.wrapInHtml(snippet);
+            }
+            snippet = dojoApi.fixLineEndings(snippet);
+            var form = $('<form method="post" target="_blank" action="' + dojoApi.configuration.url + '" />').appendTo(document.body);
+            $("<input name='snippet'>").val(window.btoa(snippet)).appendTo(form);
+
+            form.submit();
+        },
+        fixLineEndings: function (code) {
+            return code.replace(/\n/g, "&#10;");
+        },
+        showHintButton: function (element, text) {
+            element = $(element);
+
+            $("<button>")
+                .text(text)
+                .addClass("btn btn-action dojo-submit-button")
+                .insertBefore(element);
+        },
+        wrapInHtml: function(snippet) {
+            var result = ('<!DOCTYPE html>\n'+
+            '<html>\n'+
+            '<head>\n'+
+            '    <meta charset="utf-8">\n'+
+            '    <title>Kendo UI Snippet</title>\n\n'+
+            '    <link rel="stylesheet" href="http://cdn.kendostatic.com/kendo-version/styles/kendo.common.min.css">\n'+
+            '    <link rel="stylesheet" href="http://cdn.kendostatic.com/kendo-version/styles/kendo.rtl.min.css">\n'+
+            '    <link rel="stylesheet" href="http://cdn.kendostatic.com/kendo-version/styles/kendo.default.min.css">\n'+
+            '    <link rel="stylesheet" href="http://cdn.kendostatic.com/kendo-version/styles/kendo.dataviz.min.css">\n'+
+            '    <link rel="stylesheet" href="http://cdn.kendostatic.com/kendo-version/styles/kendo.dataviz.default.min.css">\n'+
+            '    <link rel="stylesheet" href="http://cdn.kendostatic.com/kendo-version/styles/kendo.mobile.all.min.css">\n\n'+
+            '    <script src="http://code.jquery.com/jquery-1.9.1.min.js"></script>\n'+
+            '    <script src="http://cdn.kendostatic.com/kendo-version/js/kendo.all.min.js"></script>\n'+
+            '</head>\n'+
+            '<body>\n  \n'+
+            snippet+ '\n' +
+            '</body>\n'+
+            '</html>').replace(/kendo-version/g, dojoApi.configuration.currentKendoVersion);
+            return result;
+        }
+    };
+
+    return dojoApi;
+})(jQuery);
+
 $(function(){
     $(".toggle-nav").click(function() {
         $("#page-search").removeClass("search-visibility");
         $("#page-inner-content").removeClass("move-inner-content");
         $("#page-nav").toggleClass("nav-visibility");
     });
-});
 
-$(function(){
     $(".show-search").click(function() {
         $("#page-nav").removeClass("nav-visibility");
         $("#page-search").toggleClass("search-visibility");
         $("#page-inner-content").toggleClass("move-inner-content");
     });
+
+    $(".prettyprint[lang=html]").each(function() {
+        dojoApi.showHintButton(this, "Edit this example");
+    });
+
+    $("body").on("click", ".dojo-submit-button", function(e) {
+        e.preventDefault();
+        var snippet = $(this).next().text();
+        dojoApi.post(snippet);
+    });
 });
 
-$
+
